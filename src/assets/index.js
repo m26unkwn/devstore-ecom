4c8521656b59dbc22f1955199dd747aaa6300acf
m26unkwn <shukla9915294376@gmail.com>
2022-03-18 18:34:43 +0530

    assets:added assests

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+export { default as cart } from "./cart.svg";
+export { default as devLogo } from "./Devlogo.svg";
+export { default as wishlist } from "./wishlist.svg";
+export { default as burger } from "./Burger.svg";
+export { default as Sticker } from "./Stickers.png";
+export { default as Tees } from "./TeesDev.png";
+export { default as Hat } from "./Hat.png";
+export { default as HeartIcon } from "./Heart.svg";
+export { default as CartIcon } from "./CartIcon.svg";
+export { default as RemoveIcon } from "./Remove.svg";
+export { default as Eye } from "./eyes.svg";
+export { default as disableEye } from "./disableEye.svg";
+
+//
