bb6fe3f3f35af6beef7469a7f3497f347f49ef16
m26unkwn <shukla9915294376@gmail.com>
2022-04-03 23:18:19 +0530

    Removed Error

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,11 @@
 export { default as FilterProducts } from "./filterProducts/FilterProducts";
 export { default as PrivateRoute } from "./privateRoute/index";
-export { default as Profile } from "./profile/index";
-export { default as Cart } from "./cart/index";
-
 export { default as CategoryProducts } from "./categoryPage/CategoryProducts";
 export { default as SingleProduct } from "./singleProduct/SingleProduct";
 export { Filter, AllProducts } from "./productList";
-export { default as Navbar } from "./navbar/index";
 export { default as Wishlist } from "./wishlist/Wishlist";
 export { HomeProducts, Category, Hero } from "./home";
 export { Login, Signup } from "./auth";
+export { default as Profile } from "./profile/index";
+export { default as Navbar } from "./navbar";
+export { default as Cart } from "./cart";
