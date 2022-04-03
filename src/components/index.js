2b35680b48cbb35fee554be0467a6f57eaf3ab9c
m26unkwn <shukla9915294376@gmail.com>
2022-04-03 22:53:00 +0530

    AUTH ERROR

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as FilterProducts } from "./filterProducts/FilterProducts";
 export { default as PrivateRoute } from "./privateRoute/index";
 export { default as Profile } from "./profile/index";
 export { default as Cart } from "./cart/index";
-export { Login, Signup } from "./auth";
 
 export { default as CategoryProducts } from "./categoryPage/CategoryProducts";
 export { default as SingleProduct } from "./singleProduct/SingleProduct";
@@ -10,3 +9,4 @@ export { Filter, AllProducts } from "./productList";
 export { default as Navbar } from "./navbar/index";
 export { default as Wishlist } from "./wishlist/Wishlist";
 export { HomeProducts, Category, Hero } from "./home";
+export { Login, Signup } from "./auth";
