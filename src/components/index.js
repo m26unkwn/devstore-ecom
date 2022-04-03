1ff76c6161de59ef074a845079528bdcab0148be
m26unkwn <shukla9915294376@gmail.com>
2022-04-03 22:47:23 +0530

    DONE WITH bux fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as FilterProducts } from "./filterProducts/FilterProducts";
 export { default as PrivateRoute } from "./privateRoute/index";
-export { Login, Signup } from "./auth";
 export { default as Profile } from "./profile/index";
 export { default as Cart } from "./cart/index";
 export { default as CategoryProducts } from "./categoryPage/CategoryProducts";
@@ -9,3 +8,4 @@ export { Filter, AllProducts } from "./productList";
 export { default as Navbar } from "./navbar/index";
 export { default as Wishlist } from "./wishlist/Wishlist";
 export { HomeProducts, Category, Hero } from "./home";
+export { Login, Signup } from "./auth";
