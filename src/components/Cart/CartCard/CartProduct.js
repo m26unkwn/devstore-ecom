b75e9e70af0b77f23c2a2210acb0c9799ea9f3e3
m26unkwn <getmonushukla@gmail.com>
2022-05-29 22:38:47 +0530

    FIX: CASE ERROR

---
+++ src/components/Cart/CartCard/CartProduct.js
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Delete, Negative, Positive } from "../../../assets";
+import { ReactComponent as StarIcon } from "../../../assets/Star.svg";
+
+import { useData, useAuth } from "../../../Context";
+import { handlers } from "../../../utils/handlers";
+import Tooltip from "../../tooltip/Tooltip";
+
+const CartProduct = (props) => {
+  const { id, img, title, desc, price, prevPrice, discPrice, qty, product } =
+    props;
+
+  const {
+    authState: { token },
+  } = useAuth();
+
+  const {
+    dispatch,
+    state: { wishlistItems },
+  } = useData();
+
+  const [loading, setLoading] = useState(false);
+
+  let isProducInWishlist = wishlistItems.some((item) => item._id === id);
+
+  return (
+    <div className='pd-card-container' style={{ background: "white" }}>
+      <div className='card-img-wrapper'>
+        <Link to={`/products/${product._id}`}>
+          <img
+            src={img}
+            className='pd-img card-img vertical-card-img'
+            alt={`${title} + img`}
+          />
+        </Link>
+      </div>
+      <div className='badge pd-badge'>
+        <button
+          disabled={loading}
+          onClick={() =>
+            handlers.removefromCart(id, token, product, dispatch, setLoading)
+          }
+          className='btn btn-icon'>
+          <Tooltip info='Remove From Cart'>
+            <img src={Delete} alt='remove-product-icon' />
+          </Tooltip>
+        </button>
+      </div>
+      <div className='pd-content'>
+        <h1 className='pd-heading card-heading'>{title}</h1>
+        <p className='pd-desc'>{desc}</p>
+        <div className='pd-price'>
+          <p className='crnt-price'>₹{price}</p>
+          <p className='prev-price'>₹{prevPrice}</p>
+          <p className='discount'>{discPrice}%</p>
+        </div>
+        <p className='price rating'>
+          {product.rating} <StarIcon fill='orange' />
+        </p>
+        <div className='pd-quantity-action flex ai-center jc-between'>
+          <span>Quantity:</span>
+          <button
+            disabled={Number(qty) === 1 || loading}
+            onClick={() =>
+              handlers.updateQuantity(
+                id,
+                token,
+                dispatch,
+                setLoading,
+                "decrement"
+              )
+            }
+            className={
+              Number(qty) === 1
+                ? "btn btn-disabled btn-icon icon-primary-color "
+                : "btn btn-icon icon-primary-color "
+            }>
+            <img src={Negative} alt='increment-product-quantity' />
+          </button>
+          <span className='quantity-view flex ai-center jc-center'>{qty}</span>
+          <button
+            onClick={() =>
+              handlers.updateQuantity(
+                id,
+                token,
+                dispatch,
+                setLoading,
+                "increment"
+              )
+            }
+            disabled={Number(qty) === 10 || loading}
+            className={
+              Number(qty) === 10
+                ? "btn btn-disabled btn-icon icon-primary-color "
+                : "btn btn-icon icon-primary-color "
+            }>
+            <img src={Positive} alt='increment-product-quantity' />
+          </button>
+        </div>
+        <div className='pd-card-action pd-card-btn'>
+          <button
+            disabled={loading}
+            onClick={() =>
+              handlers.moveToWishlist(
+                id,
+                token,
+                product,
+                dispatch,
+                setLoading,
+                isProducInWishlist
+              )
+            }
+            className='btn outline-primary'>
+            Move to wishlist
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartProduct;
---
+++ src/components/Cart/CartValue.jsx
@@ -0,0 +1,56 @@
+import { useNavigate } from "react-router-dom";
+import { useData } from "../../Context";
+import { cartValueCalc } from "./utils/cart-value";
+
+const CartValue = () => {
+  const {
+    state: { cartItems },
+  } = useData();
+
+  const cartValueData = cartValueCalc(cartItems);
+
+  const navigate = useNavigate();
+  const placeOrder = () => {
+    navigate("/cart/checkout", { state: { cartValue: cartValueData } });
+  };
+
+  return (
+    <div className='cart-checkout'>
+      <div className='card-container'>
+        <div className='card-head'>
+          <b>Price Details</b>
+        </div>
+        <div className='card-divider'></div>
+        <div className='card-content'>
+          <div className='item-details flex jc-between'>
+            <h4>Price ({cartItems.length} items )</h4>
+            <span className='price'>₹ {cartValueData.price}</span>
+          </div>
+          <div className='item-details flex jc-between'>
+            <h4>Discount</h4>
+            <span className='price'>₹ {cartValueData.discount}</span>
+          </div>
+          <div className='item-details flex jc-between'>
+            <h4>Delivery Charges</h4>
+            <span className='price'>₹0</span>
+          </div>
+          <div className='card-divider'></div>
+          <div className='total-amount'>
+            <div className='item-details flex jc-between'>
+              <h3>TOTAL AMOUNT</h3>
+              <h4>₹{cartValueData.totalprice}</h4>
+            </div>
+          </div>
+          <div className='card-divider'></div>
+          <div className='cart-action flex jc-center'>
+            <button className='btn' onClick={placeOrder}>
+              PLACE ORDER
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartValue;
